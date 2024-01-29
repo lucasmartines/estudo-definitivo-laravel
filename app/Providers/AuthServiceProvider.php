3bc4d7ee4a93a5ab6857391a6ce0c14685488d99
lucasmartines <39931785+lucasmartines@users.noreply.github.com>
2024-01-29 12:18:59 -0300

    resolver um bug estranho
    resolver um bug estranho...

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Schema::defaultStringLength(191); 
+
+
     }
 }
